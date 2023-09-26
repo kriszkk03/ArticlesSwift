59d667d8a6bcd6ecb3745fb90a21f3aa35c7fd82
kriszkk03 <44733842+kriszkk03@users.noreply.github.com>
2023-09-26 09:28:32 +0200

    init

---
+++ ArticlesWithAlgolia/ArticlesWithAlgoliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticlesWithAlgoliaApp.swift
+//  ArticlesWithAlgolia
+//
+//  Created by Tóth Krisztián on 2023. 09. 26..
+//
+
+import SwiftUI
+
+@main
+struct ArticlesWithAlgoliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
